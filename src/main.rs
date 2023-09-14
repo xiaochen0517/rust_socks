ec832f49813c86d1b493de0dcaddb91b3e9fccfd
mochen <xiaochen0517@qq.com>
2023-09-14 20:55:47 +0800

    function edit

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() {
     // 循环监听
     loop {
         let (stream, addr) = listener.accept().await.expect("连接失败");
-        println!("接收到连接: {:?}", addr);
+        println!("接收到请求: {:?}", addr);
         // 使用tokio创建一个新的任务
         tokio::spawn(async move {
             // 处理连接
@@ -40,26 +40,20 @@ async fn handshake(connection: Connection) -> Result<Connection, String> {
     let Connection { mut buf, mut stream } = connection;
     let data_length = stream.read(&mut buf).await.map_err(|err| err.to_string())?;
     println!("读取数据: {:x?}", &buf[..data_length]);
-    // 解析数据
-    let version = &buf[0];
-    if *version != 0x05u8 {
-        // 不是socks5协议
-        println!("不是socks5协议");
-        return Err("不是socks5协议".to_string());
-    }
-    // 获取认证方法数量
-    let method_count = &buf[1];
-    // 获取认证方法
-    let method = &buf[2..2 + *method_count as usize];
-    // 将认证方法使用大端序转换为数字
-    let method = u16::from_be_bytes(method.try_into().expect("转换失败"));
-    println!("认证方法字节数: {}，认证方法: {:?}", method_count, method);
-    // 返回握手成功
+    check_version(&buf[0])?;
+    let _method_length = check_method_length(&buf[1])?;
     let response = [0x05, 0x00];
-    stream.write(&response).await.expect("写入数据失败");
-    stream.flush().await.expect("刷新数据失败");
+    stream.write(&response).await.map_err(|err| err.to_string())?;
+    stream.flush().await.map_err(|err| err.to_string())?;
     println!("发送数据: {:x?}", &response);
     Ok(Connection { buf, stream })
+}
+
+fn check_method_length(length: &u8) -> Result<usize, String> {
+    if *length <= 0 {
+        return Err("方法数长度字节数不能小于0".to_string());
+    }
+    Ok(*length as usize)
 }
 
 async fn forward(connection: Connection) -> Result<Connection, String> {
@@ -67,5 +61,75 @@ async fn forward(connection: Connection) -> Result<Connection, String> {
     // 读取请求数据
     let data_length = stream.read(&mut buf).await.map_err(|err| err.to_string())?;
     println!("读取数据: {:x?}", &buf[..data_length]);
+    check_version(&buf[0])?;
+    let _socks_cmd = parse_socks_cmd(&buf[1])?;
+    let socks_address_type = parse_socks_address_type(&buf[3])?;
+    parse_socks_address(socks_address_type, &buf[4..])?;
     Ok(Connection { buf, stream })
+}
+
+#[derive(Debug)]
+enum SocksCMD {
+    Connect,
+    Bind,
+    UdpAssociate,
+}
+
+#[derive(Debug)]
+enum SocksAddressType {
+    IPv4,
+    Domain,
+    IPv6,
+}
+
+fn parse_socks_cmd(cmd: &u8) -> Result<SocksCMD, String> {
+    return match *cmd {
+        0x01u8 => {
+            Ok(SocksCMD::Connect)
+        }
+        0x02u8 => {
+            Ok(SocksCMD::Bind)
+        }
+        0x03u8 => {
+            Ok(SocksCMD::UdpAssociate)
+        }
+        _ => {
+            Err("不支持的命令".to_string())
+        }
+    };
+}
+
+fn parse_socks_address_type(types: &u8) -> Result<SocksAddressType, String> {
+    return match *types {
+        0x01u8 => {
+            Ok(SocksAddressType::IPv4)
+        }
+        0x03u8 => {
+            Ok(SocksAddressType::Domain)
+        }
+        0x04u8 => {
+            Ok(SocksAddressType::IPv6)
+        }
+        _ => {
+            Err("不支持的地址类型".to_string())
+        }
+    };
+}
+
+fn parse_socks_address(types: SocksAddressType, data_arrays: &[u8]) -> Result<(), String> {
+    println!("解析地址类型: {:?}", types);
+    match types {
+        SocksAddressType::IPv4 => println!("解析IPv4地址: {:?}", data_arrays[..4].to_vec()),
+        SocksAddressType::Domain => {}
+        SocksAddressType::IPv6 => {}
+    }
+
+    Ok(())
+}
+
+fn check_version(version: &u8) -> Result<(), String> {
+    if *version != 0x05u8 {
+        return Err("不是socks5协议".to_string());
+    }
+    Ok(())
 }
